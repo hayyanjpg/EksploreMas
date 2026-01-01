f5541e4f5173d22a298df8404d6ffed13d14de6d
Hayyan N <hayyannashrulloh@gmail.com>
2026-01-01 23:22:02 +0700

    Update vite.config.ts

--- FrontEnd/vite.config.ts
+++ FrontEnd/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://unburdensome-increasedly-min.ngrok-free.dev",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
